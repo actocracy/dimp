3a191356f06a717fcfd2c9bfbf1834aacd82a085
chef1119 <derrekjulian@gmail.com>
2023-06-01 09:40:03 -0600

    first commit

---
+++ contracts/DIMP.sol
@@ -0,0 +1,302 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract DIMP is Ownable {
+
+    using SafeMath for uint256;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => bool) private _isWhitelisted;
+
+    address[] swapAddress;
+
+    address liquidityPoolAddr;
+    address ecosystemAddr;
+    address partnerShipAddr;
+
+    uint256 private _totalSupply = 237279209162 * 10**6;
+
+    string private constant _name = "Direct Impact Monetary Piece";
+    string private constant _symbol = "DIMP";
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Sets the values for {name} and {symbol}.
+     *
+     * The default value of {decimals} is 18. To select a different value for
+     * {decimals} you should overload it.
+     *
+     * All two of these values are immutable: they can only be set once during
+     * construction.
+     */
+    constructor(address[] memory addrs, uint256[] memory tokens, address _liquidityPoolAddr, address _ecosystemAddr, address _partnerShipAddr) {
+        uint256 totalTokens = 0;
+
+        require(_liquidityPoolAddr != address(0) && _ecosystemAddr != address(0) && _partnerShipAddr != address(0));
+
+        liquidityPoolAddr = _liquidityPoolAddr;
+        ecosystemAddr = _ecosystemAddr;
+        partnerShipAddr = _partnerShipAddr;
+
+        for (uint256 i = 0; i < addrs.length; i++) {
+          totalTokens += tokens[i];
+
+          require(addrs[i] != address(0), "addrs must contain valid addresses");
+
+          _balances[addrs[i]] = tokens[i];
+
+          emit Transfer(address(0), addrs[i], tokens[i]);
+        }
+
+        require(totalTokens == _totalSupply, "total tokens must be totalSupply");
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public pure returns (uint8) {
+        return 6;
+    }
+
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function setPairAddress(address _swapAddress) external onlyOwner {
+        swapAddress.push(_swapAddress);
+    }
+
+    function addWhitelist(address _whitelistAddr) external onlyOwner {
+        _isWhitelisted[_whitelistAddr] = true;
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * Requirements:
+     *
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``sender``'s tokens of at least
+     * `amount`.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][msg.sender];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        unchecked {
+            _approve(sender, msg.sender, currentAllowance - amount);
+        }
+
+        return true;
+    }
+
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender] + addedValue);
+        return true;
+    }
+
+    /**
+     * @dev Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
+        uint256 currentAllowance = _allowances[msg.sender][spender];
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(msg.sender, spender, currentAllowance - subtractedValue);
+        }
+
+        return true;
+    }
+
+    /**
+     * @dev Moves `amount` of tokens from `sender` to `recipient`.
+     *
+     * This internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+        uint256 feeAmount = 0;
+
+        if(!_isWhitelisted[sender]) {
+            for(uint256 i = 0; i < swapAddress.length; i++) {
+                if(recipient == swapAddress[i]) {
+                    feeAmount = (amount.mul(3)).div(100);
+                    _balances[address(this)] += feeAmount;
+                    if(_balances[address(this)] >= 10000) {
+                        _balances[partnerShipAddr] += _balances[address(this)].div(3);
+                        _balances[ecosystemAddr] += _balances[address(this)].div(3);
+                        _balances[liquidityPoolAddr] + _balances[address(this)].sub((_balances[address(this)].div(3)).mul(2));
+                        _balances[address(this)] = 0;
+                    }
+                    break;
+                }
+            }
+        }
+
+        unchecked {
+            _balances[sender] = senderBalance.sub(amount);
+        }
+        _balances[recipient] += (amount.sub(feeAmount));
+
+        emit Transfer(sender, recipient, amount);
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+}
---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,83 @@
+const { strict: assert } = require('assert');
+
+const DIMP = artifacts.require("DIMP");
+
+const Addresses = {
+  PreSeed: process.env.PRESEED_ADDRESS,
+  Seed: process.env.SEED_ADDRESS,
+  PrivateA: process.env.PRIVATEA_ADDRESS,
+  PrivateB: process.env.PRIVATEB_ADDRESS,
+  Public: process.env.PUBLIC_ADDRESS,
+  Advisors: process.env.ADVISORS_ADDRESS,
+  Team: process.env.TEAM_ADDRESS,
+  Partnership: process.env.PARTNERSHIP_ADDRESS,
+  Ecosystem: process.env.ECOSYSTEM_ADDRESS,
+  GameActivity: process.env.GAMEACTIVITY_ADDRESS,
+  LiquidityPool: process.env.LIQUIDITYPOOL_ADDRESS
+}
+
+module.exports = async function (deployer) {
+  let allocations;
+
+  const totalSupply = 237279209162n * 10n**6n;
+
+  const percentage = (percent) => (totalSupply * percent) / 1000n;
+
+    allocations = [
+      // pre-seed
+      { address: Addresses.PreSeed, tokens: percentage(5n) },
+
+      // seed
+      { address: Addresses.Seed, tokens: percentage(20n) },
+
+      // private sale 1
+      { address: Addresses.PrivateA, tokens: percentage(50n) },
+
+      // private sale 2
+      { address: Addresses.PrivateB, tokens: percentage(50n) },
+
+      // public sale
+      { address: Addresses.Public, tokens: percentage(15n) },
+
+      // advisors
+      { address: Addresses.Advisors, tokens: percentage(10n) },
+
+      // team & development
+      { address: Addresses.Team, tokens: percentage(150n) },
+
+      // partnerships
+      { address: Addresses.Partnership, tokens: percentage(60n) },
+
+      // ecosystem
+      { address: Addresses.Ecosystem, tokens: percentage(100n) },
+
+      // game activities
+      { address: Addresses.GameActivity, tokens: 71183762748n * 10n**6n },
+
+      // liquidity pool
+      { address: Addresses.LiquidityPool, tokens: percentage(540n) - 71183762748n * 10n**6n },
+
+    ];
+
+  const totalAllocated = allocations.reduce((a, b) => a + b.tokens, 0n);
+  const totalPercent = Number(totalAllocated * (100n * 1000n) / totalSupply) / 1000;
+
+  assert(totalPercent === 100, `allocations must add up to 100% but is ${totalPercent}%`);
+  assert(totalAllocated === totalSupply, `allocations must add up to ${totalSupply} but is ${totalAllocated}`);
+  // for (const k in CONTRACTS) {
+  //   assert(!!CONTRACTS[k], `env.${k} is missing`);
+  // }
+
+  await deployer.deploy(
+    DIMP,
+    allocations.map(({ address }) => address),
+    allocations.map(({ tokens }) => tokens.toString()),
+    Addresses.LiquidityPool,
+    Addresses.Ecosystem,
+    Addresses.Partnership
+  );
+
+  const dimp = await DIMP.deployed();
+
+  console.log("DIMP deployed at:", dimp.address);
+};
